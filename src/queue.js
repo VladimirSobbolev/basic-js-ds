8193c03a575cd9a88bb6a3bff066e47e44c56ef5
Vladimir Sobolev <vladimir@sobbolev.ru>
2023-04-15 16:57:41 +0300

    feat: add queue resolution

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +14,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
+  constructor() {
+    this.list = null;
+    this.queue = null;
+  //djn
+  }
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    return this.list
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
+
+  enqueue(value) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    const node = new ListNode(value);
+    if (!this.list) {
+      this.list = this.queue = node;
+    } else {
+      this.queue.next = node;
+      this.queue = node;
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    const current = this.list.value;
+    this.list = this.list.next;
+    return current;
   }
 }
 

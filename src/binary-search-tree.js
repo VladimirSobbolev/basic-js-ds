9324d20936efde3f47e67c3f45883043efb8b0ed
Vladimir Sobolev <vladimir@sobbolev.ru>
2023-04-15 17:14:14 +0300

    feat: add binary-search-tree.js resolution (root, add)

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,21 +1,36 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
-
+constructor() {
+  this.head = null
+    this.length = 0;
+ }
   root() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    return this.head
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
+  add(data) {
+    if (this.length === 0) {
+        this.head = new Node(data)
+    } else {
+        let actual = this.head;
+        while (actual.next) {
+            actual = actual.next;
+        }
+        actual.next = new Node(data);
+    }
+    this.length++;
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+
   }
 
   has(/* data */) {
